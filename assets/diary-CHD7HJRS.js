bdd536251aaf7cce1d7a1027074975cb6b0f5b94
la82i <laih8207@gmail.com>
2026-03-03 03:43:13 +0000

    Deploying to gh-pages from @ la82i/tree-front@67c48ed83ebb5a731ad47dc7282ba79c91abdd85 🚀

--- assets/diary-CHD7HJRS.js
+++
@@ -1,1 +0,0 @@
-import{aD as t}from"./index-DnARMkBH.js";const a={create:e=>t.post("/diary",e),getMyHistory:()=>t.get("/diary/me"),deleteById:e=>t.delete(`/diary/${e}`)};export{a as s};
---
+++ assets/diary-ZRj1qsK_.js
@@ -0,0 +1,1 @@
+import{aD as t}from"./index-WhMba0Iz.js";const a={create:e=>t.post("/diary",e),getMyHistory:()=>t.get("/diary/me"),deleteById:e=>t.delete(`/diary/${e}`)};export{a as s};
