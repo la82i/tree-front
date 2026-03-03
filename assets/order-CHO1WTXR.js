d5763c6c220a85d60a85eeebf1c5672d6216111b
la82i <laih8207@gmail.com>
2026-03-03 02:51:00 +0000

    Deploying to gh-pages from @ la82i/tree-front@2ad03faac56ab5242d6600967e25d1da60e7dceb 🚀

--- assets/order-CHO1WTXR.js
+++
@@ -1,1 +0,0 @@
-import{aD as e}from"./index-S0eeq0eY.js";const o={create:()=>e.post("/order"),get:()=>e.get("/order"),getAll:()=>e.get("/order/all"),remove:r=>e.delete(`/order/${r}`)};export{o as s};
--- assets/use-quasar-g_Oy1Weo.js
+++
@@ -1,1 +0,0 @@
-import{E as a,bB as r}from"./index-S0eeq0eY.js";function u(){return a(r)}export{u};
