e766dccfb267870acb4317fc8a24dd9db221ac2c
la82i <laih8207@gmail.com>
2026-03-03 03:56:35 +0000

    Deploying to gh-pages from @ la82i/tree-front@92049e0eef9da44636b3716e13001fec0639edad 🚀

---
+++ assets/mood-5MxANpna.js
@@ -0,0 +1,1 @@
+import{aD as e,aC as t}from"./index-CkgeN7BM.js";const s={getAll:()=>t.get("/mood"),create:o=>e.post("/mood",o),remove:o=>e.delete(`/mood/${o}`)};export{s};
---
+++ assets/use-quasar-DlJOVwak.js
@@ -0,0 +1,1 @@
+import{E as a,bB as r}from"./index-CkgeN7BM.js";function u(){return a(r)}export{u};
