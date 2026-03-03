72ce90153515190ed59dbb103d3c630de3d13986
la82i <laih8207@gmail.com>
2026-03-03 13:02:05 +0800

    test

--- quasar.config.js
+++ quasar.config.js
@@ -48,7 +48,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: process.env.NODE_ENV === 'production' ? '/tree-front/' : '/',
+      publicPath: '/tree-front/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
